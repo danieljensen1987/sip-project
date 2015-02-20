b1fc2da4179fe0813e57e402dd1d2344ea733111
Daniel <Daniel@10.50.137.19>
2015-02-20 11:16:57 +0100

    Startup project

---
+++ src/main/java/dk/cphbusiness/hello/Greeter.java
@@ -0,0 +1,12 @@
+package dk.cphbusiness.hello;
+
+public class Greeter {
+  public static String sayHello() {
+    return sayHello("World");
+    }
+  
+  public static String sayHello(String name) {
+    return "Hello "+name+"!";
+    }
+  
+  }
---
+++ src/test/java/dk/cphbusiness/hello/GreeterTest.java
@@ -0,0 +1,19 @@
+package dk.cphbusiness.hello;
+
+import static org.hamcrest.CoreMatchers.is;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class GreeterTest {
+  
+  @Test
+  public void testSayHello() throws Exception {
+    assertThat(Greeter.sayHello(), is("Hello World!"));
+    }
+  
+  @Test
+  public void testSayHelloKurt() throws Exception {
+    assertThat(Greeter.sayHello("Kurt"), is("Hello Kurt!"));
+    }
+  
+  }
