99d193de39dc0431d9f887f1ac31e1b187ab0d51
Jonas <Jonas@10.50.130.182>
2015-03-02 12:16:04 +0100

    getProposals (SubjectHandler.class) added
    J.P.

--- src/main/java/dk/cphbusiness/handlers/SubjectHandler.java
+++ src/main/java/dk/cphbusiness/handlers/SubjectHandler.java
@@ -35,15 +35,8 @@ public class SubjectHandler implements HttpHandler {
         switch (method) {
             case "GET":
                 try {
-                    System.out.println("Du er her!");
-                    Student s2 = new Student("1");
-                    Student s1 = new Student("2");
-
-                    List al = new ArrayList();
-                    al.add(s1);
-                    al.add(s2);
-
-                    response = gson.toJson(al);
+                    
+                    response = gson.toJson(facade.getProposals());
 
                 } catch (NumberFormatException nfe) {
                     response = "Id is not a number";
