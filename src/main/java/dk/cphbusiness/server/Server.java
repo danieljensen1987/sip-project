dfd2530d59c5a8f8a88e1dbafe8d3c21bf87c9f2
Daniel <Daniel@10.50.137.10>
2015-03-05 11:17:24 +0100

    Daniel & Bettina & Jonas

--- src/main/java/dk/cphbusiness/server/Server.java
+++ src/main/java/dk/cphbusiness/server/Server.java
@@ -1,22 +1,16 @@
 package dk.cphbusiness.server;
 
-import dk.cphbusiness.entities.Student;
 import com.google.gson.Gson;
-import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
 import dk.cphbusiness.handlers.FirstRoundHandler;
 import dk.cphbusiness.handlers.SubjectHandler;
 import java.io.IOException;
-import java.io.OutputStream;
 import java.net.InetSocketAddress;
-import java.util.ArrayList;
-import java.util.List;
 
 public class Server {
 
     static int port = 8080;
     static String ip = "localhost";
-    private final Gson gson = new Gson();
 
     public void run() throws IOException {
         HttpServer server = HttpServer.create(new InetSocketAddress(ip, port), 0);
