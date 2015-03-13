bed67d0ce736228ac96fe92912af15b77f31f937
Daniel Jensen <dj@dschool.dk>
2015-03-13 09:59:26 +0100

    Refractored constructors
    Daniel & Jonas

--- src/main/java/dk/cphbusiness/entities/Selected.java
+++ src/main/java/dk/cphbusiness/entities/Selected.java
@@ -1,14 +1,21 @@
 package dk.cphbusiness.entities;
 
 public class Selected extends Subject{
+    String studentId;
     int priority;
 
-    public Selected(String title, String description, String teacher, int priority) {
+    public Selected(String title, String description, String teacher, int priority, String studentId) {
         super(title, description, teacher);
+        
+        this.studentId = studentId;
         this.priority = priority;
     }
 
     public int getPriority() {
         return priority;
-    }  
+    } 
+
+    public String getStudentId() {
+        return studentId;
+    }
 }
